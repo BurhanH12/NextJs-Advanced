2a2d7852f6e7b1ddbc77840dda86a7a1c1d01549
BurhanH12 <burhan.qsols@gmail.com>
2024-02-10 16:53:54 +0500

    build: add seo optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,15 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "NextJs Advanced",
   description: "Js Advanced Practice",
+  other: {
+    "theme-color": "#0d1117",
+    "color-scheme": "dark only",
+    "twitter-image": "https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg",
+    "twitter-card": "summary_large_image",
+    "og-image": "https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg",
+    "og-type": "website",
+    "og-url": "https://q-sols.com",
+  }
 };
 
 export default function RootLayout({
